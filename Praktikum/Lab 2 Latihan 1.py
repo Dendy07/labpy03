01788a410d0fa9f5ab6123437405d639e9e0d569
Dendy Saputra <dendyscik@gmail.com>
2021-11-15 14:27:27 +0700

    Menambahkan file '/c/Users/acer/labpy03/Praktikum/Lab 2 Latihan 1.py'

---
+++ Praktikum/Lab 2 Latihan 1.py
@@ -0,0 +1,7 @@
+print("Program Bilangan Terbesar dari 2 Buah Bilangan yang Diinputkan")
+A=int(input("Masukkan Bilangan Pertama :"))
+B=int(input("Masukkan Bilangan Kedua :"))
+if A>B:
+    print(A, "Terbesar dari 2 bilangan yang diinputkan")
+else:
+    print(B, "Terbesar dari 2 bilangan yang diinputkan")
