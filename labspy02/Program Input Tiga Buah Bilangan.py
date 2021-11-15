40f19595f7a5766ca4fb43ea701bbffe72558e4d
Dendy Saputra <dendyscik@gmail.com>
2021-11-15 14:30:37 +0700

    Menambahkan file '/c/Users/acer/labpy03/labspy02/Program Input Tiga Buah Bilangan.py'

---
+++ labspy02/Program Input Tiga Buah Bilangan.py
@@ -0,0 +1,10 @@
+print("Program Bilangan Terbesar dari 3 Buah Bilangan yang Diinputkan")
+A=int(input("Masukkan Bilangan Pertama :"))
+B=int(input("Masukkan Bilangan Kedua :"))
+C=int(input("Masukkan Bilangan Ketiga :"))
+if A>B and A>c:
+    print(A, "Terbesar dari 3 bilangan yang diinputkan")
+elif B>A and B>C:
+    print(B, "Terbesar dari 3 bilangan yang diinputkan")
+else:
+    print(C, "Terbesar dari 3 bilangan yang diinputkan")
